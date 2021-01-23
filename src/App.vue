6c2a2c12eaf962299e7df094cbb5eb6cf8a09e53
COSER277 <chenbigboss@outlook.com>
2021-01-23 10:49:20 +0800

    根页面全占视图大小

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,9 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: rgb(246, 247, 252);
+  width: 100%;
+  height: 100vh;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
